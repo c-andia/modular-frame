1f092ce3fe6db885c0b86220a2504168a6e70380
c-andia <christopher.alex.andia@gmail.com>
2026-05-29 11:28:06 -0400

    Update monitor_GPIO.py

--- camera-code/monitor_GPIO.py
+++ camera-code/monitor_GPIO.py
@@ -8,9 +8,10 @@ import pathlib
 import subprocess
 import pyudev
 
-folder_name="/mnt/Lexar/Photos"
+## Initialize the pins
 
 GPIO.setmode(GPIO.BOARD)
+
 greenPin=36
 yellowPin=38
 inPin=40
@@ -23,9 +24,11 @@ wait_for_release = False
 MONITORING = True
 
 def check_usb():
-
+	
     context = pyudev.Context()
 
+	## Check for any usb drive connection (take_photo.py will still only save photos to specific drives)
+	
     for device in context.list_devices(subsystem='block', DEVTYPE='partition'):
         if device.get('ID_BUS') == 'usb':
             return True
@@ -44,6 +47,9 @@ def flash_led(colPin, flashDuration):
 		time.sleep(0.2)
 
 try:
+
+	## Clear any stuck pins and indicate that the device is armed after booting 
+	
 	GPIO.output(greenPin, 0)
 	GPIO.output(yellowPin, 0)
 
@@ -55,18 +61,20 @@ try:
 
 		button_state = GPIO.input(inPin)
 
+		## Allow the user to press and hold the button by only triggering after it has been released
+		
 		if button_state == GPIO.LOW:
 
 			wait_for_release = True
 			GPIO.output(greenPin, 1)
 
-		if wait_for_release == False and button_state == GPIO.HIGH:
-			pass
-
 		if wait_for_release == True and button_state == GPIO.HIGH:
 
 			GPIO.output(greenPin, 0)
 			GPIO.output(yellowPin, 1)
+
+			## Check USB every time the program is run, to avoid crashes if the user has forgotten to put it back in
+			
 			usb_is_there = check_usb()
 
 			if usb_is_there == 0:
@@ -75,21 +83,31 @@ try:
 				print("USB is not connected")
 
 			else:
+
+				## Call the take_photo script in a subprocess so it can write to the drive, as this requires sudo capability   
+				
 				subprocess.run(['sudo', 'python3', 'take_photo.py'])
+
+				## Indicate that the cameras are done capturing, and give time for the USB to unmount automatically 
+
 				GPIO.output(yellowPin, 0)
 				flash_led(greenPin, 4)
 				GPIO.output(greenPin, 1)
 				time.sleep(0.4)
-
+				
 			GPIO.output(yellowPin, 0)
 			GPIO.output(greenPin, 0)
 
 			wait_for_release = False
 
 except KeyboardInterrupt:
+
 	print("interrupted")
 
 finally:
+
+	## Indicate that the program has been interrupted.
+	
 	for i in range(2):
 		flash_led(greenPin, 1)
 		flash_led(yellowPin, 1)
